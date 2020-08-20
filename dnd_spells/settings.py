cd1f0a556fc63a16a8c6016458a59d9572c3105d
Mykola Moskvin <nsmoskvin@gmail.com>
2020-08-20 14:13:18 +0300

    Settings updated.

--- dnd_spells/settings.py
+++ dnd_spells/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'spell',
-    'dnd_class'
+    'dnd_class',
+    'django_extensions',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [

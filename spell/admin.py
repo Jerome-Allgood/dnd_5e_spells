7caef674d594576452a71a8713092d47d3db2ab5
Mykola Moskvin <nsmoskvin@gmail.com>
2020-08-20 20:56:08 +0300

    Spell admin updated.

--- spell/admin.py
+++ spell/admin.py
@@ -6,7 +6,13 @@ from spell.models import Spell
 @admin.register(Spell)
 class SpellAdmin(admin.ModelAdmin):
 	list_display = (
-		'name', 'level', 'school', 'text', 'casting_time', 'range', 'materials',
-		'components', 'ritual', 'concentration', 'duration', 'source'
+		'name', 'name_en', 'level', 'school', 'text', 'casting_time',
+		'range', 'materials', 'materials_en', 'components', 'ritual',
+		'concentration', 'duration', 'source'
 	)
 	search_fields = ('name', 'source', 'name_en')
+	fields = (
+		'name', 'name_en', 'level', 'school', 'text', 'text_en', 'casting_time',
+		'range', 'components', 'materials', 'materials_en',
+		('ritual', 'concentration'), 'duration', 'source'
+	)

fc388c0a89adf0c29d5ad03dab20e1dd2faf3fc3
Mykola Moskvin <nsmoskvin@gmail.com>
2020-10-16 16:16:08 +0300

    Serializers updated.

--- spell/api/serializers.py
+++ spell/api/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework	 import serializers
-from spell.models import Spell
+from spell.models import Spell, Component
+
+
+class ComponentSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Component
+		fields = ('name', )
 
 
 class SpellRuSerializer(serializers.ModelSerializer):
+	components = ComponentSerializer(read_only=True, many=True)
+
 	class Meta:
 		model = Spell
-		fields = ('id', 'name', 'level', 'school', 'text', 'casting_time',
-		          'range', 'components', 'materials', 'duration', 'ritual',
-		          'concentration')
+		fields = (
+			'id', 'name', 'level', 'school', 'text', 'casting_time', 'range',
+			'components', 'materials', 'duration', 'ritual', 'concentration'
+		)
